9328189ee01189746693d57f87b7452f385b2cc4
Philip Chang <pienote1234567@gmail.com>
2022-02-17 14:15:42 -0500

    Testing Navigation

---
+++ src/components/Navigation.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Navbar, Container, Nav, NavLink} from 'react-bootstrap';
+
+class Navigation extends React.Component {
+    render() {
+        <Navbar>
+            <Container>
+                <Navbar.Brand href="/"> Hi </Navbar.Brand>
+                <Nav className="me-auto" activeKey={ window.location.pathname}>
+                    <NavLink activeClassName="active"> There </NavLink>
+                    <NavLink activeClassName="active"> My </NavLink>
+                    <NavLink activeClassName="active"> Name </NavLink>
+                </Nav>
+            </Container>
+        </Navbar>
+    }
+}
