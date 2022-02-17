ce16c03d61129db6b0bf26465f7e19fb49e6fca7
Philip Chang <pienote1234567@gmail.com>
2022-02-17 14:40:42 -0500

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 import Navigation from './components/Navigation';
 
@@ -7,7 +8,18 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Navigation/>
-        Hi there.
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Phil
+        </a>
       </header>
     </div>
   );
