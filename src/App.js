242e6103b361f374f0bbefe1f94acc9f41a3e17f
Philip Chang <pienote1234567@gmail.com>
2022-06-06 10:30:55 -0400

    Added MUI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Navigation/>
-        Heyheyhey
+        <BrowserRouter>
+          <Navigation/>
+          <Routes>
+            <Route />
+          </Routes>
+        </BrowserRouter>
       </header>
     </div>
   );
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,21 +1,13 @@
 import React from 'react';
-import {Navbar, Container, Nav, NavLink} from 'react-bootstrap';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
 
-class Navigation extends React.Component {
-    render() {
-        return (
-            <Navbar sticky="top" fixed="top" bg="dark" variant="dark">
-                <Container>
-                    <Navbar.Brand href="/"> Hi </Navbar.Brand>
-                    <Nav className="me-auto" activeKey={ window.location.pathname}>
-                        <NavLink activeClassName="active"> There </NavLink>
-                        <NavLink activeClassName="active"> My </NavLink>
-                        <NavLink activeClassName="active"> Name </NavLink>
-                    </Nav>
-                </Container>
-            </Navbar>
-        );
-    }
+export default function Navigation() {
+    return (
+        <AppBar position="static">
+            <Toolbar variant="dense">
+                Hello
+            </Toolbar>
+        </AppBar>
+    );
 }
-
-export default Navigation;
