c7683bcb3da03afd3fec9d181733ab1fd66b6145
Philip Chang <pienote1234567@gmail.com>
2022-07-27 10:50:14 -0400

    Edited website

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
-
+import pic from './pic.png';
 
 export default function Home() {
 
     return(
         <div className="Home">
-            <h1> Welcome! </h1>
+            <h1> Welcome to my website! </h1>
+            <img src={pic} alt="Picture of me" style={{width:"50%", height:"50%"}}/>
             <h2> Hi, my name is Phil. <br/> I'm a master's student at RPI studying Information Technology expected to graduate in May 2023. </h2>
         </div>
     );
