60956e108a0bb1872583b996e796b027a6766d51
pienote <pienote1234567@gmail.com>
2023-07-11 21:40:55 -0400

    Added twitter project link

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,7 +6,7 @@ function Projects() {
         <div className="Projects">
             <ul>
                 <li>
-                <b>Twitter Ukraine Project</b> - clustered 10,000 tweets about the Russian-Ukrainian by performing different clustering methods on the MiniLM embeddings: k-means and HDBSCAN. Then it compares the coherence scores of the two different methods. Created using Python and utilizes libraries such as numpy, pandas, nltk, and scikit-learn.
+                <b>Twitter Ukraine Project</b> - clustered 10,000 tweets about the Russian-Ukrainian by performing different clustering methods on the MiniLM embeddings: k-means and HDBSCAN. Then it compares the coherence scores of the two different methods. Created using Python and utilizes libraries such as numpy, pandas, nltk, and scikit-learn. (<a target="_blank" href="https://github.com/pienote/DataAnalytics_A6_Philip_Chang">LINK</a>)
                 </li>
                 <li>
                 <b>Crypto Arbitrage Dashboard</b> - a dashboard that takes advantage of the price differential of the same cryptocurrencies on two different decentralized exchanges and attempts to make a trade based on the difference for the purpose of profit. Made using HTML/JavaScript, as well as React, Node.js, and web3.js.
