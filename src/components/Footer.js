3e4e9ff2df79560ab2ca55a5bb52cf76aea0bd6f
pienote <pienote1234567@gmail.com>
2023-08-06 07:46:07 -0400

    Updated so information

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,16 +7,16 @@ import {faLinkedin, faGithub} from '@fortawesome/free-brands-svg-icons'
 export default function Footer() {
     return (
         <div className="Footer">
-        <footer style={{backgroundColor: "orange", width: "100%"}}>
-            Contacts:
-            <br/>
-            <a href="https://www.linkedin.com/in/philip5chang" style={{color: "#0A66C2", textDecoration: "none", textAlign: "center"}}>
-                <FontAwesomeIcon icon={faLinkedin}/>    
-            </a>
-            <a href="https://github.com/pienote" style={{color: "#000000", textDecoration: "none", textAlign: "center"}}>
-                <FontAwesomeIcon icon={faGithub}/>    
-            </a>
-        </footer>
+            <footer style={{backgroundColor: "orange", width: "100%"}}>
+                Contacts:
+                <br/>
+                <a href="https://www.linkedin.com/in/philip5chang" style={{color: "#0A66C2", textDecoration: "none", textAlign: "center"}}>
+                    <FontAwesomeIcon icon={faLinkedin}/>    
+                </a>
+                <a href="https://github.com/pienote" style={{color: "#000000", textDecoration: "none", textAlign: "center"}}>
+                    <FontAwesomeIcon icon={faGithub}/>    
+                </a>
+            </footer>
         </div>
     );
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,7 +3,7 @@ import {AppBar, Toolbar, Button, Link} from '@mui/material';
 
 export default function Navigation() {
     return (
-        <AppBar position="static">
+        <AppBar position="static" style={{background: "#0f0"}}>
             <Toolbar variant="dense">
                 <Button href="/" component={Link} color="inherit"> Home </Button>
                 <Button href="#/projects/" component={Link} color="inherit"> Projects </Button>
