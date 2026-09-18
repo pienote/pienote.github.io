7dc57187028a71755ff613f30bb6418ad52ed5b1
Phil C <pienote1234567@gmail.com>
2026-09-18 14:58:52 -0400

    fix: disabled jekyll

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
 
+	export const prerender = true;
+
 	let { children } = $props();
 </script>
 
--- vite.config.ts
+++ vite.config.ts
@@ -21,9 +21,6 @@ export default defineConfig({
 				precompress: false,
 				strict: true,
 			}),
-			paths: {
-				base: process.argv.includes('dev') ? '' : process.env.BASE_PATH as `/${string}`,
-			},
 		})
 	]
 });
